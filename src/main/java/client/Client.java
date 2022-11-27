3e21aeddd0490c4a5f9b8c3068e15e1184bd10ce
Lucas Messias <21.lucasmessias@gmail.com>
2022-11-27 16:47:43 -0300

    feat: add example

---
+++ src/main/java/client/Client.java
@@ -0,0 +1,54 @@
+package client;
+
+import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class Client {
+    static final int port = 8181;
+
+    public static void main(String[] args) {
+        try {
+            Socket socket;
+            if (args.length == 1) {
+                socket = new Socket(args[0],port);
+            } else {
+                InetAddress addr = InetAddress.getByName("localhost"); // pega o ip do servidor
+                socket = new Socket(addr,port);
+            }
+            System.out.println("Socket:" + socket);
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(
+                    new BufferedWriter(
+                            new OutputStreamWriter(socket.getOutputStream())));
+
+            // Recebe as boas vindas
+            String s = in.readLine();
+            System.out.println(s);
+
+            // interage com o usuario
+            BufferedReader teclado = new BufferedReader(
+                    new InputStreamReader(System.in));
+
+            s = teclado.readLine();
+            while (! s.toLowerCase().equals("fim")) {
+
+                out.println(s); // manda o que o usuario escreveu
+                out.flush();
+
+                s = in.readLine(); // pega a resposta do servidor
+                System.out.println(s); // mostra para o usuario
+
+                s = teclado.readLine();
+            }
+
+
+            // Termina a conexao
+            out.println("fim");
+            out.flush();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
